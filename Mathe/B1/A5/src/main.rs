53994ac6fcc9131631e401055ee1f5e9fd0783da
Tom Paßberg <passberg@rhrk.uni-kl.de>
2024-04-25 20:25:57 +0200

    first commit

---
+++ Mathe/B1/A5/src/main.rs
@@ -0,0 +1,18 @@
+use rayon::iter::{IntoParallelIterator, ParallelIterator};
+
+fn main() {
+    let teiler: Vec<u64> = vec![3,5,7,11];
+    for n in (1..=10).map(|i| 10u64.pow(i)) {
+        println!(
+            "{:11} gerade Zahlen zwischen 1 und {n:11} sind durch mindestens einen der Teiler {} teilbar.", 
+            count_numbers(n, &teiler), 
+            teiler.iter().map(|&t| t.to_string()).collect::<Vec<String>>().join(", ")
+        );
+    }
+}
+
+fn count_numbers(n: u64, teiler: &Vec<u64>) -> usize {
+    (2..=n).into_par_iter()
+        .filter(|&n| n % 2 == 0 && teiler.iter().any(|&t| n % t == 0))
+        .count()
+}   
