468d144a2d3ed3f332942b2e94c09a46d1a39dfb
Tom Paßberg <passberg@rhrk.uni-kl.de>
2024-04-29 21:02:39 +0200

    A2 code

--- Mathe/B1/A2/target/debug/deps/A2-b9e53a8d9aef7a9a.d
+++
@@ -1,5 +0,0 @@
-C:\Users\tompa\Documents\Uni\SS2024\Mathe\B1\A2\target\debug\deps\libA2-b9e53a8d9aef7a9a.rmeta: src\main.rs
-
-C:\Users\tompa\Documents\Uni\SS2024\Mathe\B1\A2\target\debug\deps\A2-b9e53a8d9aef7a9a.d: src\main.rs
-
-src\main.rs:
--- Mathe/B1/A2/target/release/A2.d
+++
@@ -1,1 +0,0 @@
-C:\Users\tompa\Documents\Uni\SS2024\Mathe\B1\A2\target\release\A2.exe: C:\Users\tompa\Documents\Uni\SS2024\Mathe\B1\A2\src\main.rs
