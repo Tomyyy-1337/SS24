2d3dea24f4d1cc20579c297188e5a4c7598ad3ef
Tom Paßberg <passberg@rhrk.uni-kl.de>
2024-05-06 23:41:33 +0200

    HPC

--- HPC/Ex01/A3/run.sh
+++ HPC/Ex01/A3/run.sh
@@ -3,9 +3,9 @@
 #SBATCH -J benchmark
 #SBATCH -t 00:10 
 #SBATCH -p skylake-96
-#SBATCH --ntasks=3
+#SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=200
+#SBATCH --mem=10
 #SBATCH -o benchmark_results.out
 #SBATCH -e errors.err
 #SBATCH --mail-type=NONE
