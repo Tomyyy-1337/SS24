1e4ba8bdd17761145449ad17349e22a0632fb217
Tom Paßberg <passberg@rhrk.uni-kl.de>
2024-05-21 17:02:25 +0200

    fix

--- Mathe/B4/A5/src/main.rs
+++ Mathe/B4/A5/src/main.rs
@@ -1,46 +1,3 @@
-<<<<<<< HEAD
-use std::vec;
-
-fn main() {
-    let input = 4;
-    let m = 2;
-    let result = partitions(input, m);
-    for r in result.iter() {
-        println!("{:?}", r);
-    }
-
-
-}
-fn partitions(n: usize, m: usize) -> Vec<Vec<Vec<usize>>> {
-    if m == 0 {
-        return vec![];
-    }
-    if m == 1 {
-        return vec![vec![(1..=n).collect::<Vec<_>>()]];
-    }
-    if n == 0 {
-        return vec![];
-    }
-    let mut result = vec![];
-
-    let mut parts = partitions(n - 1, m - 1);
-    for p in parts.iter_mut() {
-        p.push(vec![n]);
-        result.push(p.clone());
-    }
-
-    let mut parts = partitions(n - 1, m);
-    for p in parts.iter_mut() {
-        for i in 0..m {
-            let mut q = p.clone();
-            q[i].push(n);
-            result.push(q);
-        }
-    }
-
-    result
-}   
-=======
 fn main() {
     let parts = partitions(5, 3);
 
@@ -71,4 +28,3 @@ fn partitions(n: u8, m: u8) -> Vec<Vec<Vec<u8>>> {
     }
     result
 }
->>>>>>> 65b051abf57e2a569e9f5a45ea0e8f76c37bb0f9
