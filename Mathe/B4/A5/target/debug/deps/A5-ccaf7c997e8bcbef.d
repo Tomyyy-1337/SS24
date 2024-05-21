30763f7b49fa529c87e206a1896e755269eeebfa
Tom Paßberg <passberg@rhrk.uni-kl.de>
2024-05-21 17:02:34 +0200

    fix

--- Mathe/B4/A5/target/debug/deps/A5-ccaf7c997e8bcbef.d
+++
@@ -1,5 +0,0 @@
-C:\Users\tompa\Documents\TU\SS24\Mathe\B4\A5\target\debug\deps\libA5-ccaf7c997e8bcbef.rmeta: src\main.rs
-
-C:\Users\tompa\Documents\TU\SS24\Mathe\B4\A5\target\debug\deps\A5-ccaf7c997e8bcbef.d: src\main.rs
-
-src\main.rs:
